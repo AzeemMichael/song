764f33fef5215f028ef10887dfe2caa812f1c9cd
Azeem Michael <amichael76@gmail.com>
2016-05-16 13:32:11 -0400

    Create song.php

---
+++ song.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+class Song extends Thread
+{
+    private $lyric;
+    private $times;
+
+    public function __construct(string $line, int $repetitions)
+    {
+        $this->lyric = $line;
+        $this->times = $repetitions;
+    }
+
+    public function run()
+    {
+        for ($i=0; $i<$this->times; ++$i) {
+            echo $this->lyric."\r\n";
+            sleep(2);
+        }
+    }
+}
+
+$s1 = new Song('Yes I can!', 30);
+$s2 = new Song("\t\t\t\t\tNo you can't!", 20);
+
+echo "Anything you can do, I can do better!\r\n";
+echo "\t\t\t\t\tI can do anything better than you.\r\n";
+
+$s1->start();
+$s2->start();
